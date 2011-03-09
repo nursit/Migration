e86d37b1750ea375dd3b4021ca09128cdd32d3a8
Cerdic <cedric@yterium.com>
2011-03-09 14:32:19 +0000

    anonymiser l'action de suivi du status migration pour ne pas le perdre quand la base auteur est vide

--- action/migration_depuis_status.php
+++ action/migration_depuis_status.php
@@ -10,22 +10,18 @@ include_spip('inc/migration');
 include_spip('inc/actions');
 
 function action_migration_depuis_status_dist(){
-	$securiser_action = charger_fonction('securiser_action','inc');
-	$securiser_action();
+	#$securiser_action = charger_fonction('securiser_action','inc');
+	#$securiser_action();
 	
-	if (autoriser('webmestre')){
-		if (!$s = lire_migration_depuis_status())
-			ajax_retour("Echec : le site distant n'a pas réussi à se connecter, la migration a été abandonnée.");
-		else{
-			if ($s['key']!=_request('key')){
-				ajax_retour("La clé de migration n'est plus valable, recommmencez la migration");
-			}
-			else {
-				ajax_retour(migration_afficher_status($s));
-			}
+	if (!$s = lire_migration_depuis_status())
+		ajax_retour("Echec : le site distant n'a pas réussi à se connecter, la migration a été abandonnée.");
+	else{
+		if ($s['key']!=_request('key')){
+			ajax_retour("La clé de migration n'est plus valable, recommmencez la migration");
+		}
+		else {
+			ajax_retour(migration_afficher_status($s));
 		}
 	}
-	else
-		ajax_retour("Erreur de droits pour lire l'avancement de la migration");
 	exit;
 }
