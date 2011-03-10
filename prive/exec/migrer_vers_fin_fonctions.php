c6f77766cb94b3c32f09a88627667be195bad312
Cerdic <cedric@yterium.com>
2011-03-10 11:47:50 +0000

    message de fin plus explicite et complet

--- prive/exec/migrer_vers_fin_fonctions.php
+++ prive/exec/migrer_vers_fin_fonctions.php
@@ -1,3 +1,13 @@
 <?php
 include_spip('inc/migration');
+
+function affiche_champs_ignores($ignores){
+	if (!is_array($ignores) OR !count($ignores))
+		return '';
+	$res = '';
+	foreach($ignores as $table=>$champs){
+		$res .= "Table $table&nbsp;: ".implode(', ',$champs);
+	}
+	return $res;
+}
 ?>
