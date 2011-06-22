3b1cb3b19e1ca0f75296ba8de05ff0ce6e415d48
Ben <ben@rezo.net>
2011-06-22 10:59:58 +0200

    first git commit ?

--- lang/migration_fr.php
+++ lang/migration_fr.php
@@ -1,5 +1,5 @@
 <?php
-// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// -- This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
