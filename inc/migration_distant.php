bafcb99b590a6d69f8566ffa7c96227939687f5c
Cerdic <cedric@yterium.com>
2019-02-20 16:10:01 +0100

    indentation

--- inc/migration_distant.php
+++ inc/migration_distant.php
@@ -8,16 +8,20 @@
  *                                                                         *
  *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
  *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
-\***************************************************************************/
+ * \***************************************************************************/
 
-if (!defined('_ECRIRE_INC_VERSION')) { return;
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
 }
 
-if (!defined('_INC_DISTANT_VERSION_HTTP')) { define('_INC_DISTANT_VERSION_HTTP', 'HTTP/1.0');
+if (!defined('_INC_DISTANT_VERSION_HTTP')) {
+	define('_INC_DISTANT_VERSION_HTTP', 'HTTP/1.0');
 }
-if (!defined('_INC_DISTANT_CONTENT_ENCODING')) { define('_INC_DISTANT_CONTENT_ENCODING', 'gzip');
+if (!defined('_INC_DISTANT_CONTENT_ENCODING')) {
+	define('_INC_DISTANT_CONTENT_ENCODING', 'gzip');
 }
-if (!defined('_INC_DISTANT_USER_AGENT')) { define('_INC_DISTANT_USER_AGENT', 'SPIP-' .$GLOBALS['spip_version_affichee']. ' (' .$GLOBALS['home_server']. ')');
+if (!defined('_INC_DISTANT_USER_AGENT')) {
+	define('_INC_DISTANT_USER_AGENT', 'SPIP-' . $GLOBALS['spip_version_affichee'] . ' (' . $GLOBALS['home_server'] . ')');
 }
 
 // les fonctions non buggues natives du core
@@ -62,7 +66,8 @@ function migration_recuperer_page(
 
 	// Accepter les URLs au format feed:// ou qui ont oublie le http://
 	$url = preg_replace(',^feed://,i', 'http://', $url);
-	if (!preg_match(',^[a-z]+://,i', $url)) { $url = 'http://'.$url;
+	if (!preg_match(',^[a-z]+://,i', $url)) {
+		$url = 'http://' . $url;
 	}
 
 	if ($taille_max == 0) {
@@ -74,17 +79,19 @@ function migration_recuperer_page(
 	if (!empty($datas)) {
 		$get = 'POST';
 		list($type, $postdata) = prepare_donnees_post($datas, $boundary);
-		$datas = $type . 'Content-Length: '.strlen($postdata)."\r\n\r\n".$postdata;
+		$datas = $type . 'Content-Length: ' . strlen($postdata) . "\r\n\r\n" . $postdata;
 	}
 
 	// dix tentatives maximum en cas d'entetes 301...
-	for ($i=0; $i<10; $i++) {
+	for ($i = 0; $i < 10; $i++) {
 		$url = migration_recuperer_lapage($url, $trans, $get, $taille_max, $datas, $refuser_gz, $date_verif, $uri_referer);
-		if (!$url) { return false;
+		if (!$url) {
+			return false;
 		}
 		if (is_array($url)) {
 			list($headers, $result) = $url;
-			return ($get_headers ? $headers."\n" : '').$result;
+
+			return ($get_headers ? $headers . "\n" : '') . $result;
 		} else {
 			spip_log("recuperer page recommence sur $url", 'migration');
 		}
@@ -96,7 +103,16 @@ function migration_recuperer_page(
 // si $trans est null -> on ne veut que les headers
 // si $trans est une chaine, c'est un nom de fichier pour ecrire directement dedans
 // http://doc.spip.org/@migration_recuperer_lapage
-function migration_recuperer_lapage($url, $trans = false, $get = 'GET', $taille_max = 1048576, $datas = '', $refuser_gz = false, $date_verif = '', $uri_referer = '') {
+function migration_recuperer_lapage(
+	$url,
+	$trans = false,
+	$get = 'GET',
+	$taille_max = 1048576,
+	$datas = '',
+	$refuser_gz = false,
+	$date_verif = '',
+	$uri_referer = ''
+) {
 	// $copy = copier le fichier ?
 	$copy = (is_string($trans) and strlen($trans) > 5); // eviter "false" :-)
 
@@ -107,9 +123,11 @@ function migration_recuperer_lapage($url, $trans = false, $get = 'GET', $taille_
 	}
 
 	// ouvrir la connexion et envoyer la requete et ses en-tetes
-	list($f, $fopen) = migration_init_http($get, $url, $refuser_gz, $uri_referer, $datas, _INC_DISTANT_VERSION_HTTP, $date_verif);
+	list($f, $fopen) = migration_init_http($get, $url, $refuser_gz, $uri_referer, $datas, _INC_DISTANT_VERSION_HTTP,
+		$date_verif);
 	if (!$f) {
 		spip_log("ECHEC migration_init_http $url", 'migration');
+
 		return false;
 	}
 
@@ -125,6 +143,7 @@ function migration_recuperer_lapage($url, $trans = false, $get = 'GET', $taille_
 			// les actions liberticides de l'empire du milieu
 			if ($headers) {
 				spip_log("HTTP status $headers pour $url", 'migration');
+
 				return false;
 			} elseif ($result = @file_get_contents($url)) {
 				return array('', $result);
@@ -135,24 +154,27 @@ function migration_recuperer_lapage($url, $trans = false, $get = 'GET', $taille_
 		if (!is_array($headers)) { // cas Location
 			fclose($f);
 			include_spip('inc/filtres');
+
 			return suivre_lien($url, $headers);
 		}
 		$headers = join('', $headers);
 	}
 
-	if ($trans === null) { return array($headers, '');
+	if ($trans === null) {
+		return array($headers, '');
 	}
 
 	// s'il faut deballer, le faire via un fichier temporaire
 	// sinon la memoire explose pour les gros flux
 
 	$gz = preg_match(",\bContent-Encoding: .*gzip,is", $headers) ?
-		(_DIR_TMP.md5(uniqid(mt_rand())).'.tmp.gz') : '';
+		(_DIR_TMP . md5(uniqid(mt_rand())) . '.tmp.gz') : '';
 
 #	spip_log("entete ($trans $copy $gz)\n$headers", 'migration');
 	$result = migration_recuperer_body($f, $taille_max, $gz ? $gz : ($copy ? $trans : ''));
 	fclose($f);
-	if (!$result) { return array($headers, $result);
+	if (!$result) {
+		return array($headers, $result);
 	}
 
 	// Decompresser au besoin
@@ -175,11 +197,12 @@ function migration_recuperer_body($f, $taille_max = 1048576, $fichier = '') {
 	$result = '';
 	if ($fichier) {
 		$fp = spip_fopen_lock($fichier, 'w', LOCK_EX);
-		if (!$fp) { return false;
+		if (!$fp) {
+			return false;
 		}
 		$result = 0; // on renvoie la taille du fichier
 	}
-	while (!feof($f) and $taille<$taille_max) {
+	while (!feof($f) and $taille < $taille_max) {
 		$res = fread($f, 16384);
 		$taille += strlen($res);
 		if ($fp) {
@@ -192,6 +215,7 @@ function migration_recuperer_body($f, $taille_max = 1048576, $fichier = '') {
 	if ($fp) {
 		spip_fclose_unlock($fp);
 	}
+
 	return $result;
 }
 
@@ -211,13 +235,13 @@ function migration_recuperer_entetes($f, $date_verif = '') {
 	$headers = array();
 	$not_modif = $location = false;
 	while ($s = trim(fgets($f, 16384))) {
-		$headers[]= $s."\n";
+		$headers[] = $s . "\n";
 		preg_match(',^([^:]*): *(.*)$,i', $s, $r);
-		list(,$d, $v) = $r;
+		list(, $d, $v) = $r;
 		if (strtolower(trim($d)) == 'location' and $status >= 300 and $status < 400) {
 			$location = $v;
 		} elseif ($date_verif and ($d == 'Last-Modified')) {
-			if ($date_verif>=strtotime($v)) {
+			if ($date_verif >= strtotime($v)) {
 				//Cas ou la page distante n'a pas bouge depuis
 				//la derniere visite
 				$not_modif = true;
@@ -225,10 +249,13 @@ function migration_recuperer_entetes($f, $date_verif = '') {
 		}
 	}
 
-	if ($location) { return $location;
+	if ($location) {
+		return $location;
 	}
-	if ($status != 200 or $not_modif) { return $status;
+	if ($status != 200 or $not_modif) {
+		return $status;
 	}
+
 	return $headers;
 }
 
@@ -237,7 +264,15 @@ function migration_recuperer_entetes($f, $date_verif = '') {
 // retourne le descripteur sur lequel lire la reponse
 //
 // http://doc.spip.org/@migration_init_http
-function migration_init_http($method, $url, $refuse_gz = false, $referer = '', $datas = '', $vers = 'HTTP/1.0', $date = '') {
+function migration_init_http(
+	$method,
+	$url,
+	$refuse_gz = false,
+	$referer = '',
+	$datas = '',
+	$vers = 'HTTP/1.0',
+	$date = ''
+) {
 	$user = $via_proxy = $proxy_user = '';
 	$fopen = false;
 
@@ -245,30 +280,35 @@ function migration_init_http($method, $url, $refuse_gz = false, $referer = '', $
 	$host = $t['host'];
 	if ($t['scheme'] == 'http') {
 		$scheme = 'http';
-$noproxy = '';
+		$noproxy = '';
 	} elseif ($t['scheme'] == 'https') {
 		$scheme = 'ssl';
-$noproxy = 'ssl://';
-		if (!isset($t['port']) || !($port = $t['port'])) { $t['port'] = 443;
+		$noproxy = 'ssl://';
+		if (!isset($t['port']) || !($port = $t['port'])) {
+			$t['port'] = 443;
 		}
 	} else {
 		$scheme = $t['scheme'];
-$noproxy = $scheme.'://';
+		$noproxy = $scheme . '://';
 	}
 	if (isset($t['user'])) {
 		$user = array($t['user'], $t['pass']);
 	}
 
-	if (!isset($t['port']) || !($port = $t['port'])) { $port = 80;
+	if (!isset($t['port']) || !($port = $t['port'])) {
+		$port = 80;
 	}
-	if (!isset($t['path']) || !($path = $t['path'])) { $path = '/';
+	if (!isset($t['path']) || !($path = $t['path'])) {
+		$path = '/';
 	}
-	if (@$t['query']) { $path .= '?' .$t['query'];
+	if (@$t['query']) {
+		$path .= '?' . $t['query'];
 	}
 
-	$f = migration_lance_requete($method, $scheme, $user, $host, $path, $port, $noproxy, $refuse_gz, $referer, $datas, $vers, $date);
+	$f = migration_lance_requete($method, $scheme, $user, $host, $path, $port, $noproxy, $refuse_gz, $referer, $datas,
+		$vers, $date);
 	if (!$f) {
-	  // fallback : fopen
+		// fallback : fopen
 		if (!_request('tester_proxy')) {
 			$f = @fopen($url, 'rb');
 			spip_log("connexion vers $url par simple fopen", 'migration');
@@ -282,11 +322,25 @@ $noproxy = $scheme.'://';
 }
 
 // http://doc.spip.org/@migration_lance_requete
-function migration_lance_requete($method, $scheme, $user, $host, $path, $port, $noproxy, $refuse_gz = false, $referer = '', $datas = '', $vers = 'HTTP/1.0', $date = '') {
+function migration_lance_requete(
+	$method,
+	$scheme,
+	$user,
+	$host,
+	$path,
+	$port,
+	$noproxy,
+	$refuse_gz = false,
+	$referer = '',
+	$datas = '',
+	$vers = 'HTTP/1.0',
+	$date = ''
+) {
 
 	$proxy_user = '';
 	$http_proxy = need_proxy($host);
-	if ($user) { $user = urlencode($user[0]).':'.urlencode($user[1]);
+	if ($user) {
+		$user = urlencode($user[0]) . ':' . urlencode($user[1]);
 	}
 
 	if ($http_proxy) {
@@ -295,34 +349,37 @@ function migration_lance_requete($method, $scheme, $user, $host, $path, $port, $
 			. "$host" . (($port != 80) ? ":$port" : '') . $path;
 		$t2 = @parse_url($http_proxy);
 		$first_host = $t2['host'];
-		if (!($port = $t2['port'])) { $port = 80;
+		if (!($port = $t2['port'])) {
+			$port = 80;
 		}
 		if ($t2['user']) {
 			$proxy_user = base64_encode($t2['user'] . ':' . $t2['pass']);
 		}
 	} else {
-		$first_host = $noproxy.$host;
+		$first_host = $noproxy . $host;
 	}
 
 	$f = @fsockopen($first_host, $port);
 	spip_log("Recuperer $path sur $first_host:$port par $f", 'migration');
-	if (!$f) { return false;
+	if (!$f) {
+		return false;
 	}
 
 	$site = $GLOBALS['meta']['adresse_site'];
 
 	$req = "$method $path $vers\r\n"
-	. "Host: $host\r\n"
-	. 'User-Agent: ' . _INC_DISTANT_USER_AGENT . "\r\n"
-	. ($refuse_gz ? '' : ('Accept-Encoding: ' . _INC_DISTANT_CONTENT_ENCODING . "\r\n"))
-	. (!$site ? '' : "Referer: $site/$referer\r\n")
-	. (!$date ? '' : 'If-Modified-Since: ' . (gmdate('D, d M Y H:i:s', $date)  ." GMT\r\n"))
-	. (!$user ? '' : ('Authorization: Basic ' . base64_encode($user) ."\r\n"))
-	. (!$proxy_user ? '' : "Proxy-Authorization: Basic $proxy_user\r\n")
-	. (!strpos($vers, '1.1') ? '' : "Keep-Alive: 300\r\nConnection: keep-alive\r\n");
+		. "Host: $host\r\n"
+		. 'User-Agent: ' . _INC_DISTANT_USER_AGENT . "\r\n"
+		. ($refuse_gz ? '' : ('Accept-Encoding: ' . _INC_DISTANT_CONTENT_ENCODING . "\r\n"))
+		. (!$site ? '' : "Referer: $site/$referer\r\n")
+		. (!$date ? '' : 'If-Modified-Since: ' . (gmdate('D, d M Y H:i:s', $date) . " GMT\r\n"))
+		. (!$user ? '' : ('Authorization: Basic ' . base64_encode($user) . "\r\n"))
+		. (!$proxy_user ? '' : "Proxy-Authorization: Basic $proxy_user\r\n")
+		. (!strpos($vers, '1.1') ? '' : "Keep-Alive: 300\r\nConnection: keep-alive\r\n");
 
 #	spip_log("Requete\n$req", 'migration');
 	fputs($f, $req);
 	fputs($f, $datas ? $datas : "\r\n");
+
 	return $f;
 }
